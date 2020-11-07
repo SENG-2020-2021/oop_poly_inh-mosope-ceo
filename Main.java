f3822b3cba89c06f4896e839ef74398985f07c85
mosope-ceo <71437445+mosope-ceo@users.noreply.github.com>
2020-11-07 22:45:13 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,18 +21,14 @@ class Main {
           float pBase = scan.nextFloat();
           System.out.println("Input the height of your Parallelogram");
           float pHeight = scan.nextFloat();
-          Parallelogram p = new Parallelogram(pBase, pHeight);
-          
+         
+	  Parallelogram p = new Parallelogram(pBase, pHeight);
           Quadilateral q = p;
-
-        
           System.out.println("Input the dimension of the first pair of your Parallelogram");
           q.setLength(scan.nextFloat());
           System.out.println("Input the second pair of your Parallelogram");
           q.setBreadth(scan.nextFloat());
-          
           System.out.printf("The area of the parallelogram with base %.2f and height %.2f is %.2f", pBase, pHeight, p.Area());
-
           System.out.println("");
           System.out.printf("The perimeter of the parallelogram with First Pair: %.2f and SecondPair: %.2f is %.2f", q.getLength(), q.getBreadth(), q.calculatePerimeter());
           System.out.println("");
@@ -43,18 +39,24 @@ class Main {
 
           //this should be in the Trapezoid case
     case 'T':    
-          System.out.println("Calculation for a Trapezoid");
-          System.out.println("Input first side");
-          float a = scan.nextFloat();
-          System.out.println("Input second side");
-          float b = scan.nextFloat();
-          System.out.println("Input third side");
-          float c = scan.nextFloat();
-          System.out.println("Input fourth side");
-          float d = scan.nextFloat();
-          Trapezoid t = new Trapezoid(a, b, c, d);
-          System.out.printf("The perimeter of the Trapezoid with First Side: %.2f, Second Side: %.2f, Third Side: %.2f, and Fourth Side: %.2f is %.2f", a, b, c, d, t.calculatePerimeter()); 
-          break;
+	  System.out.println("Calculation for a Trapezoid");
+	  System.out.println("Input first side");
+	  float a = scan.nextFloat();
+	  System.out.println("Input second side");
+	  float b = scan.nextFloat();
+	  System.out.println("Input third side");
+	  float c = scan.nextFloat();
+	  System.out.println("Input fourth side");
+	  float d = scan.nextFloat();
+	  System.out.println("Input height");
+	  Float ht = scan.nextFloat();
+		      
+	  Trapezoid t = new Trapezoid(a, b, c, d,ht);
+	  System.out.printf("The perimeter of the Trapezoid with First Side: %.2f, Second Side: %.2f, Third Side: %.2f, and Fourth Side: %.2f is %.2f", a, b, c, d, t.calculatePerimeter()); 
+	  System.out.println(" ");
+	  System.out.printf("The area of the Trapezoid with First Side: %.2f, Second Side: %.2f, Third Side: %.2f, and Fourth Side: %.2f and hheight of: %.2f is %.2f", a, b, c, d, ht, t.calculateArea()); 
+	  break;
+	
          
          
           //Rhombus case
@@ -169,16 +171,21 @@ class Trapezoid extends Quadilateral{
   private float side2;
   private float side3;
   private float side4;
-  public Trapezoid (float a, float b, float c, float d){
+  private float height;
+  public Trapezoid (float a, float b, float c, float d,float ht){
     side1 = a;
     side2 = b;
     side3 = c;
     side4 = d;
+    height = ht;
   }
 
   public float calculatePerimeter(){
     return side1+side2+side3+side4;
   }
+public float calculateArea() {
+	return ((side1+side2)/2)*height;
+}
 }
 
 
